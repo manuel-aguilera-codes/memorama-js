a11d5055c59533d6c147013f4d96b92b682f7310
Manuel Aguilera <manuel.aguilera.vr@gmail.com>
2026-05-30 11:27:30 -0600

    final con local Storage de pares abiertos, sus colores y el tiempo soFar

--- src/class/game.js
+++ src/class/game.js
@@ -19,19 +19,20 @@ class Game {
     this.idElement = idElement;
     this.container = document.getElementById(idElement);
     this.boxes = [];
+    this.paresAbiertos = 0;
+    this.inicio = null;
     this.createBoxes();
     this.paintBoxes();
     this.divs = document.querySelectorAll("#game .box");
     this.bloqueado = false;
     this.tarjeta1 = null;
     this.tarjeta2 = null;
-    this.paresAbiertos = 0;
-    this.inicio = null;
+
     this.setEventListeners();
   }
 
   createRandomColors() {
-    //Creamos array con 15 colores aleatorios
+    //Creamos array con cantidad de colores (string)  aleatorios igual a la mitad de cartas totales
     let randomColors = [];
 
     for (let i = 0; i < (this.cols * this.rows) / 2; i++) {
@@ -50,25 +51,62 @@ class Game {
   }
 
   createBoxes() {
-    let ColorIndex = 0;
-    let arrayColors = this.createRandomColors();
-
-    for (let row = 0; row < this.rows; row++) {
-      for (let col = 0; col < this.cols; col++) {
-        let box = new Box(arrayColors[ColorIndex]);
-        ColorIndex++;
+    // Si hay datos en localStorage, se usan para crear las boxes
+    let box;
+    if (localStorage.getItem("boxes") !== null) {
+      let boxesFromLocalStorage = JSON.parse(localStorage.getItem("boxes"));
+      boxesFromLocalStorage.map((thisBox) => {
+        box = new Box(thisBox.color);
+        box.isMatched = thisBox.isMatched;
         this.boxes.push(box);
+      });
+    } else {
+      // Creamos array de objetos de clase Box desde cero
+      let index = 0;
+      let arrayColors = this.createRandomColors();
+
+      for (let row = 0; row < this.rows; row++) {
+        for (let col = 0; col < this.cols; col++) {
+          box = new Box(arrayColors[index]);
+          index++;
+          this.boxes.push(box);
+        }
       }
     }
+
+    if (localStorage.getItem("timeSoFar") !== null) {
+      this.inicio = Date.now() - parseInt(localStorage.getItem("timeSoFar"));
+    }
+
+    if (localStorage.getItem("paresAbiertos") !== null) {
+      this.paresAbiertos = parseInt(localStorage.getItem("paresAbiertos"));
+    }
+
+    this.saveArrayBoxesInLocalStorage();
+  }
+
+  saveArrayBoxesInLocalStorage() {
+    let arrayBoxesForLocalStorage = this.boxes.map((box) => {
+      return {
+        color: box.color,
+        isMatched: box.isMatched,
+      };
+    });
+    localStorage.setItem("boxes", JSON.stringify(arrayBoxesForLocalStorage));
+    localStorage.setItem("timeSoFar", Date.now() - this.inicio);
+    localStorage.setItem("paresAbiertos", this.paresAbiertos);
   }
 
   paintBoxes() {
     this.setGridTemplate();
-    let index = 0;
-    this.boxes.map((box) => {
+    this.boxes.forEach((box, index) => {
       let newBoxDiv = document.createElement("div");
       newBoxDiv.classList.add("box");
       newBoxDiv.dataset.color = box.color;
+      newBoxDiv.dataset.index = index;
+      if (box.isMatched === true) {
+        newBoxDiv.style.backgroundColor = box.color;
+      }
       this.container.append(newBoxDiv);
     });
   }
@@ -79,22 +117,45 @@ class Game {
   }
 
   static askUserSize() {
-    let rows = prompt("Introduzca el número de filas");
-    let cols = prompt("Introduzca número de columnas");
+    let rows;
+    let cols;
 
-    while ((rows * cols) % 2 !== 0) {
-      rows = prompt(
-        "Introduzca el número de filas nuevamente, el número de cartas debe ser par",
-      );
-      cols = prompt(
-        "Introduzca número de columnas nuevamente, el número de cartas debe ser par",
-      );
+    if (
+      localStorage.getItem("rows") !== null &&
+      localStorage.getItem("cols") !== null
+    ) {
+      rows = parseInt(localStorage.getItem("rows"));
+      cols = parseInt(localStorage.getItem("cols"));
+    } else {
+      rows = prompt("Introduzca el número de filas");
+      cols = prompt("Introduzca número de columnas");
+
+      while ((rows * cols) % 2 !== 0) {
+        rows = prompt(
+          "Introduzca el número de filas nuevamente, el número de cartas debe ser par",
+        );
+        cols = prompt(
+          "Introduzca número de columnas nuevamente, el número de cartas debe ser par",
+        );
+      }
+
+      localStorage.setItem("rows", rows);
+      localStorage.setItem("cols", cols);
     }
 
     return {
       rows: rows,
       cols: cols,
     };
+  }
+
+  static resetGame() {
+    localStorage.removeItem("rows");
+    localStorage.removeItem("cols");
+    localStorage.removeItem("boxes");
+    localStorage.removeItem("timeSoFar");
+    localStorage.removeItem("paresAbiertos");
+    location.reload();
   }
 
   comprobarIgualdad = (event) => {
@@ -113,10 +174,13 @@ class Game {
       this.bloqueado = true;
 
       if (this.tarjeta1.dataset.color === this.tarjeta2.dataset.color) {
+        this.paresAbiertos = this.paresAbiertos + 1;
+        this.boxes[parseInt(this.tarjeta1.dataset.index)].isMatched = true;
+        this.boxes[parseInt(this.tarjeta2.dataset.index)].isMatched = true;
+        this.saveArrayBoxesInLocalStorage();
         this.tarjeta1 = null;
         this.tarjeta2 = null;
         this.bloqueado = false;
-        this.paresAbiertos = this.paresAbiertos + 1;
       } else {
         setTimeout(() => {
           this.tarjeta1.style.backgroundColor = "black";
--- src/main.js
+++ src/main.js
@@ -3,6 +3,12 @@ import "../sass/main.scss";
 import Game from "./class/game";
 import Box from "./class/box";
 
+let resetButton = document.getElementById("reset");
+
+resetButton.addEventListener("click", () => {
+  Game.resetGame();
+});
+
 let rowsCols = Game.askUserSize();
 
 let juego = new Game(rowsCols.rows, rowsCols.cols, "game");
